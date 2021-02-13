5021b148cc21058f268edde8f2a5aa220dda3a3d
Kyler Renneker <krenneker@peakactivity.com>
2021-02-13 16:40:03 -0500

    test

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.json({ limit: "10mb" }));
 
 const smtpTransport = nodemailer.createTransport({
-  service: "gmail",
+  service: "Gmail",
   // port: 465,
   auth: {
     user: "krenneker16@gmail.com",
